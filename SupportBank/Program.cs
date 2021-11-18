99496e625dd3a5ebbebabe10cb347a7c713974b4
harrybradshaw <harrylewisbradshaw@googlemail.com>
2021-11-18 10:33:23 +0000

    Initial

---
+++ SupportBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string path = @"C:\Work\Training\SupportBank\Transactions2014.csv";
+        }
+    }
+}
