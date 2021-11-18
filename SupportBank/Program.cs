2a2551bfc32542c669bc199d5088178e4ec5d62b
harrybradshaw <harrylewisbradshaw@googlemail.com>
2021-11-18 10:48:39 +0000

    Reads in all transactions and stores in ReadFile object.

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SupportBank
 {
+    class ReadFile
+    {
+        public List<string> lines = new List<string>();
+        public string fname;
+        public ReadFile(string fname)
+        {
+            string[] allLines = System.IO.File.ReadAllLines(fname);
+            foreach (var line in allLines)
+            {
+                lines.Add(line);
+            }
+        } 
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             string path = @"C:\Work\Training\SupportBank\Transactions2014.csv";
+            ReadFile myFile = new ReadFile(path);
+            foreach (var line in myFile.lines)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
